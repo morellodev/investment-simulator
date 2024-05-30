79aa8d4a0dbd4592f06414eb699616d94f9e4006
Dennis Morello <dennismorello@gmail.com>
2024-05-30 09:04:22 +0200

    chore: switch to js tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 import forms from "@tailwindcss/forms";
-import type { Config } from "tailwindcss";
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [forms()],
-} satisfies Config;
+};
