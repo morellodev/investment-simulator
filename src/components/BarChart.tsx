5eb27cc8d732da206b3f38eb50ccc97dacc62aef
Dennis Morello <dennismorello@gmail.com>
2024-06-01 19:49:12 +0200

    feat: add tooltips on chart

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import { PHI } from "../utils/math";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { Currency } from "./Currency";
 
 type Props = {
   series: number[];
@@ -19,13 +21,32 @@ export const BarChart: FC<Props> = ({ series }) => {
   return (
     <svg className="fill-current size-full">
       {coords.map(([x, y], i) => (
-        <rect
-          key={i}
-          width={`${barWidth}%`}
-          height={`${y}%`}
-          x={`${x}%`}
-          y={`${100 - y}%`}
-        />
+        <TooltipPrimitive.Root key={i}>
+          <TooltipPrimitive.Trigger asChild>
+            <rect
+              width={`${barWidth}%`}
+              height={`${y}%`}
+              x={`${x}%`}
+              y={`${100 - y}%`}
+            />
+          </TooltipPrimitive.Trigger>
+          <TooltipPrimitive.Portal>
+            <TooltipPrimitive.Content
+              sideOffset={5}
+              className="px-1.5 py-1 md:px-3 md:py-2 font-medium bg-white rounded shadow"
+            >
+              <div className="flex flex-col items-center">
+                <span className="text-sm text-zinc-600 md:text-md">
+                  Year {i + 1}
+                </span>
+                <span className="text-md md:text-base">
+                  <Currency value={series[i]} />
+                </span>
+              </div>
+              <TooltipPrimitive.Arrow className="fill-white" />
+            </TooltipPrimitive.Content>
+          </TooltipPrimitive.Portal>
+        </TooltipPrimitive.Root>
       ))}
     </svg>
   );
