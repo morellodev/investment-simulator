874ac7bd8d4116606407e71bb14c568e284a4136
Dennis Morello <dennismorello@gmail.com>
2024-06-02 13:04:16 +0200

    feat: add html plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
   plugins: [
     react({
       babel: {
         plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          deployUrl: process.env.VERCEL_PROJECT_PRODUCTION_URL,
+        },
       },
     }),
   ],
