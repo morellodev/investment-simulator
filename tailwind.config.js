33e90dcba02ebc4417dbb3cf1b631e96641fe25e
Dennis Morello <dennismorello@gmail.com>
2024-06-04 20:29:43 +0200

    fix: use proper dark mode config options in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import animate from "tailwindcss-animate";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
+  darkMode: "selector",
   content: ["./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
