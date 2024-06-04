c3788232c7937ec93e0b559793b13b89cdbcc152
Dennis Morello <dennismorello@gmail.com>
2024-06-04 21:56:07 +0200

    chore: remove radix tooltip

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
@@ -6,8 +5,6 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TooltipProvider>
-      <App />
-    </TooltipProvider>
+    <App />
   </StrictMode>
 );
