14f8232bea2ee4ae384440f3baab072a7ed22a1e
Dennis Morello <dennismorello@gmail.com>
2024-06-12 11:44:12 +0200

    chore: remove vite-plugin-html

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
 import sitemap from "vite-plugin-sitemap";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -10,13 +9,6 @@ export default defineConfig({
     react({
       babel: {
         plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          deployUrl: process.env.VERCEL_PROJECT_PRODUCTION_URL,
-        },
       },
     }),
     sitemap({
