8ec7623b7c2e149ef5f4ae2d2bfd1c3edd399c48
Dennis Morello <dennismorello@gmail.com>
2025-02-06 21:08:29 +0100

    feat: refine layout and styling for improved dialog presentation

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -36,7 +36,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed bottom-0 sm:bottom-[unset] left-[50%] sm:top-[50%] z-50 grid w-full sm:max-w-lg translate-x-[-50%] sm:translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg rounded-b-none sm:rounded-b-lg",
+        "fixed bottom-0 sm:bottom-[unset] left-[50%] sm:top-[50%] z-50 grid w-full sm:max-w-lg translate-x-[-50%] sm:translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-bottom-full sm:data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-0 sm:data-[state=open]:slide-in-from-top-[48%] rounded-lg rounded-b-none sm:rounded-b-lg",
         className,
       )}
       {...props}
