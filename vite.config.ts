7bf5ec6926aa74331210a414c4c69d17a8ae5dd2
Dennis Morello <dennismorello@gmail.com>
2025-03-29 09:57:59 +0100

    feat: update TypeScript configuration and dependencies for improved build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import sitemap from "vite-plugin-sitemap";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
     react({
       babel: {
         plugins: ["babel-plugin-react-compiler"],
@@ -17,4 +16,9 @@ export default defineConfig({
       hostname: "https://money.morello.dev",
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
