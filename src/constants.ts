806f5c29a79b354bf4113bbb8b2eebde869ec9f2
Dennis Morello <dennismorello@gmail.com>
2026-04-22 16:59:27 +0200

    fix: require at least 1 year for investment duration

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,6 @@ export const initialInvestmentRange: Range<number> = [0, 100_000];
 
 export const monthlyContributionRange: Range<number> = [0, 5_000];
 
-export const investmentDurationYearsRange: Range<number> = [0, 99];
+export const investmentDurationYearsRange: Range<number> = [1, 99];
 
 export const annualInflationRange: Range<number> = [0, 0.08];
